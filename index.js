c99cf6cd6512905bfb682b8a4aa5a45045015bb1
GabrielHenriqueFerreira <130231469+GabrielHenriqueFerreira@users.noreply.github.com>
2025-03-13 11:21:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3220;
 
-
 const API_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiZTMwMDM5NzVlZTk3Y2Y0YmYwMjRiNzVkMGMxN2Q1NjdkOGU5NzBjZjFkZjMzNDA2MTA2MTAzNDNiYjk5OWE0MGExNTljOWJiNTJlZjE5NTQiLCJpYXQiOjE3NDEyNjUyMjkuMDY1NDA4LCJuYmYiOjE3NDEyNjUyMjkuMDY1NDEsImV4cCI6MTc3MjgwMTIyOS4wMjQxNDQsInN1YiI6IjllNTNmZTlmLWMxM2QtNDNkYy1hYjc0LWRiNjZjOTIyY2MwNyIsInNjb3BlcyI6WyJzaGlwcGluZy1jYWxjdWxhdGUiXX0.E3tDiYWJt4PRoVRlfz7c08upQECvWDCOSsgSo-HKnwP0pBJ9EgQHksV3rpRnybKJX3iAci-En2Cx0wjC6nFnl75LwOQ4NoMoemB2a2zR4ODGye8zVFCrln7dG1N7ETJVI3mlMHPRCyyHFClbFk1TcXGIuFSYLhgE2j1x_9dtzdOT-8SbkNEET2--HG08-ElkL2KFOAjONe7yNNPBTn2UNYeZjcT0ceTJATfA4EAEq0EJIRzdWXqh0Tjwg2yEsIcEBvAaVxCFllSn6_aWPcSOC4r9zxrrhYwKPElVC7ZBBRgyvZWmceOQ0EXZFL3Dv5BDLza-asNKzIUaTW9tR1tnvZMyQ3deId91eC9QhfL2yt28rABT0hHqSkCNXis0vDTFd4iMzATzVy0c1pAq2s2iUipiwosceLwlh82_ul8Gi0RoImrXvdImud_MhYcefRU5XFDil5Uvb_N2VNfW_nW5tEw8Y-sItsG5_Ab5aEoFbReN2MYIrx0iLF--Td4Q_vWgBZPGgRFIQ2WUpqpPBJfzYAlrauEcs-d4wrDYtKC0BqE1su0GKU4APFDebBZD-358UiET8ppIHyEifE-G_T7eQvD2SC0k2M02ra9ysZM2etstOFNJ_CeNrcw2-Giusl5x8kWqFGUt-LR9jNIcsZLF2ns-RivapOAD_0edlR2kbVc'; // Substitua pela sua chave API do Melhor Envio
 
-app.use(cors());
+// Configuração do CORS para permitir requisições do GitHub Pages
+const corsOptions = {
+    origin: 'https://gabrielhenriqueferreira.github.io', // Permite apenas requisições deste domínio
+    methods: 'POST', // Permite apenas o método POST
+    optionsSuccessStatus: 200 // Para navegadores mais antigos
+};
+
+app.use(cors(corsOptions)); // Aplica as configurações do CORS
 app.use(express.json());
 
 app.post('/calcular-frete', async (req, res) => {
@@ -52,7 +58,7 @@ app.post('/calcular-frete', async (req, res) => {
         );
 
         console.log('Resposta da API do Melhor Envio:', response.data);
-        res.json(response.data);
+        res.json(response.data); // Retorna a resposta da API do Melhor Envio
     } catch (error) {
         console.error('Erro ao calcular frete:', error.response ? error.response.data : error.message);
         res.status(500).json({ error: 'Erro ao calcular frete', details: error.response ? error.response.data : error.message });
